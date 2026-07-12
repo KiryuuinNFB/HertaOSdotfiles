95817f96602d16cabae226f95054f140f17820e3
Kiryuuin <doraemonstichisnoon@gmail.com>
2026-07-12 21:57:20 +0700

    fixed power.sh

--- .config/waybar/power.sh
+++ .config/waybar/power.sh
@@ -8,18 +8,11 @@ choice=$(printf "$shutdown\n$reboot\n$logout" | rofi -dmenu -p "HertaOS | Power 
 
 case "$choice" in
     "$shutdown")
-        paplay ~/.config/hertaos/audio/hertashutdown.ogg &&
-        sleep 1 &&
-        hyprshutdown -t 'Shutting down...' --post-cmd 'shutdown -P 0'
-        ;;
+        paplay ~/.config/hertaos/audio/hertashutdown.ogg && sleep 1 && hyprshutdown -t 'Shutting down...' && sleep 3 && systemctl poweroff;;
+
     "$reboot")
-        paplay ~/.config/hertaos/audio/hertareboot.ogg &&
-        sleep 2 &&
-        hyprshutdown -t 'Restarting...' --post-cmd 'reboot'
-        ;;
+        paplay ~/.config/hertaos/audio/hertareboot.ogg && sleep 1 && hyprshutdown -t 'Restarting...'&& sleep 3 && systemctl reboot;;
+
     "$logout")
-        paplay ~/.config/hertaos/audio/hertalogoff.ogg &&
-        sleep 1 &&
-        hyprshutdown --vt 2
-        ;;
+        paplay ~/.config/hertaos/audio/hertalogoff.ogg && sleep 1 && hyprshutdown --vt 2;;
 esac
